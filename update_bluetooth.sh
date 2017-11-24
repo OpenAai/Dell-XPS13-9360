70c8dc1d5008cc5623c04a18447f1e759e64bd98
OpenAai <github@koppelaar.org>
2017-11-24 06:21:49 +0100

    Update update_bluetooth.sh

--- update_bluetooth.sh
+++ update_bluetooth.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-mv -vf BCM4350C5_003.006.007.0095.1703.hcd /lib/firmware/brcm/BCM-0a5c-6412.hcd
+mv -vf BCM-0a5c-6412.hcd /lib/firmware/brcm/BCM-0a5c-6412.hcd
 modprobe -r btusb; sleep 1; modprobe btusb
-
