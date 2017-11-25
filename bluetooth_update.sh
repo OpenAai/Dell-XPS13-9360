8b597b454543315fcaa68d921cbe138d4e374482
OpenAai <github@koppelaar.org>
2017-11-25 08:42:00 +0100

    Update bluetooth_update.sh

--- bluetooth_update.sh
+++ bluetooth_update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mv -vf BCM-0a5c-6412.hcd /lib/firmware/brcm/BCM-0a5c-6412.hcd
-modprobe -r btusb; sleep 1; modprobe btusb
+sudo mv -vf BCM-0a5c-6412.hcd /lib/firmware/brcm/BCM-0a5c-6412.hcd
+sudo modprobe -r btusb; sleep 1; modprobe btusb
